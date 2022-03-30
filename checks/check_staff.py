4bb7087a70a4191ac5acce592d9c87127cc3bf3c
Gangsta-Muffin <94480512+Gangsta-Muffin@users.noreply.github.com>
2022-03-30 22:50:38 +0200

    Update check_staff.py

--- checks/check_staff.py
+++ checks/check_staff.py
@@ -1,7 +1,7 @@
 import json
 
 def check_staff(ctx):
-    with open(".\\checks\\staff.json", "r") as ninjas:
-        staff = json.load(ninjas)
+    with open(".\\checks\\staff.json", "r") as members:
+        staff = json.load(members)
 
     return ctx.author.id in staff
